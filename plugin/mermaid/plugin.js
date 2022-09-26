5b80f75ba9b5a3bf8f16d7033e2d8e66aaf5d9b9
Jufeng Zhang <zjffun@gmail.com>
2022-09-27 00:02:46 +0800

    feat: optimize log error

--- plugin/mermaid/plugin.js
+++ plugin/mermaid/plugin.js
@@ -30,17 +30,16 @@ const Plugin = {
           insertSvg
         );
       } catch (error) {
-        console.error(error, { graphDefinition, el });
-        if(error.str){
+        let errorStr = "";
+        if (error?.str) {
           // From mermaid 9.1.4, error.message does not exists anymore
-          console.trace("Error with str : ",error);
-          el.innerHTML = error.str;
+          errorStr = error.str;
         }
-        if(error.message){
-          console.trace("Error with message : ",error);
-          el.innerHTML = error.message;
+        if (error?.message) {
+          errorStr = error.message;
         }
-
+        console.error(errorStr, { error, graphDefinition, el });
+        el.innerHTML = errorStr;
       }
     });
   },
